e2ffe5d026eb3d90adb1e1daa21599d797e9562f
Nostalgia <67553431+ImnotUnknownTH@users.noreply.github.com>
2024-12-26 15:52:07 +0700

    #

--- conn.php
+++ conn.php
@@ -5,7 +5,7 @@ $password = "";
 $dbname = "student";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=student", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "เชื่อมต่อข้อมูลสำเร็จ";
